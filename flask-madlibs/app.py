083700b47b9a7236b519349518f43ca13be76882
000xzhou <xzhou92@gmail.com>
2023-12-15 10:31:16 -0500

    added make own story mode

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -1,51 +1,48 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, jsonify
 from stories import *
 
 app = Flask(__name__)
 
-
+# home
 @app.route('/')
 def index():
    return render_template("index.html", story_templates = story_templates)
 
+# to input story form keys
 @app.route('/story_form', methods=["POST"])
 def story_form():
    selected_option = request.form['story']
-   print(selected_option)
-   return render_template(f"story_form.html", selected_story = selected_option)
+   story_instance = story_templates[selected_option]
+   words_list = story_instance.prompts
+   print(words_list)
+   return render_template(f"story_form.html", selected_story = selected_option, words_list = words_list)
 
+# to display story
 @app.route('/story', methods=["POST"])
 def storytelling():
-   place = request.form.get("place")
-   noun = request.form.get("noun")
-   verb = request.form.get("verb")
-   adjective = request.form.get("adjective")
-   plural_noun = request.form.get("plural_noun")
    story_value = request.form.get('story_value')
    selected_story = story_templates.get(story_value)
-   # finish_story = story1.generate({"place": place, "verb": verb, "adjective": adjective, "plural_noun": plural_noun, "noun": noun})
-   finish_story = selected_story.generate({
-        "place": place, "noun": noun, "verb": verb, 
-        "adjective": adjective, "plural_noun": plural_noun
-   })
+   all_data = {}
+   for key in request.form:
+        all_data[key] = request.form.get(key)
+   finish_story = selected_story.generate(all_data)
    return render_template("story.html", finish_story = finish_story)
 
 # make your own story
 @app.route('/make_your_own_story')
 def make_your_own():
    return render_template("make_your_own_story.html")
-@app.route('/own_story_lines', methods=["POST"])
-def own_story_lines():
-   line1 = request.form.get("line-1")
-   line2 = request.form.get("line-2")
-   line3 = request.form.get("line-3")
-   line4 = request.form.get("line-4")
-   story_name = request.form.get("story_name")
-   
-   story_text = f"{line1} {{place}}, {line2} {{adjective}} {{noun}}. {line3} {{verb}} {{plural_noun}}. {line4}."
-   # should get keys like "place", etc from form... and then put it in a form... maybe later 
-   story_templates[story_name] = Story(
-   ["place", "noun", "verb", "adjective", "plural_noun"], story_text)
-   return redirect('/')
 
-app.run(debug=True)
+@app.route('/submit_own_story', methods=['POST'])
+def submit_story():
+    data = request.json
+    story_title = data.get('story_title')
+    input_keys = data.get('input_keys')
+    input_story = " ".join(data.get('input_story'))
+    
+    new_story = Story(input_keys, input_story)
+    story_templates[story_title] = new_story
+    return jsonify({"status": "success", "input_keys": input_keys, "input_story": input_story})
+
+if __name__ == "__main__":
+   app.run(debug=True)
--- flask-madlibs/static/app.js
+++ flask-madlibs/static/app.js
@@ -1,30 +1,70 @@
+// create story
+input_keys = [];
+input_story = [];
+function newElement(inputFieldId) {
+  let inputType = document.createElement("div");
+  let inputValue = document.getElementById(inputFieldId).value;
+  let inputItem = document.createTextNode(inputValue);
+  // console.log("Input Value from", inputFieldId, ":", inputValue);
+  inputType.appendChild(inputItem);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("pharagrapth").appendChild(inputType);
+  }
+  document.getElementById("myLine").value = "";
+  document.getElementById("myKey").value = "";
+
+  // adding into array
+  if (inputFieldId == "myKey") {
+    input_keys.push(inputValue);
+    input_story.push("{" + inputValue + "}");
+  } else {
+    input_story.push(inputValue);
+  }
+}
+
+// when submit story button is pressed submit inputs
 document
-  .getElementById("story_form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const form = this;
-    let input1Value = form["place"].value;
-    let input2Value = form["noun"].value;
-    let input3Value = form["verb"].value;
-    let input4Value = form["adjective"].value;
-    let input5Value = form["plural_noun"].value;
-    if (
-      input1Value == "" ||
-      input2Value == "" ||
-      input3Value == "" ||
-      input4Value == "" ||
-      input5Value == ""
-    ) {
-      return console.error("Inputs cannot be empty");
-    } else if (
-      input1Value.length < 3 ||
-      input2Value.length < 3 ||
-      input3Value.length < 3 ||
-      input4Value.length < 3 ||
-      input5Value.length < 3
-    ) {
-      return console.error("Inputs need to be least 3 characters long");
+  .getElementById("submit_own_story")
+  .addEventListener("click", function () {
+    story_title = document.getElementById("story_title").value;
+    if (story_title == "") {
+      alert("You must have a title!");
+    } else if (input_story.length == 0) {
+      alert("You must add at least 1 line!");
+    } else if (input_keys.length == 0) {
+      alert("You must add at least 1 key!");
     } else {
-      return form.submit();
+      sendDataToPython(story_title);
+      window.location.assign("/");
     }
   });
+
+function sendDataToPython(story_title) {
+  // URL of the Python server's endpoint
+  const url = "/submit_own_story";
+
+  // Data to be sent
+  const data = {
+    input_keys: input_keys,
+    input_story: input_story,
+    story_title: story_title,
+  };
+
+  // Use fetch API to send data
+  fetch(url, {
+    method: "POST", // or 'PUT'
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
